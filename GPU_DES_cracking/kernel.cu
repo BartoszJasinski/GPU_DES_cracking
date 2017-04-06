fb0cb0038ffa7fd9f614351cbe0950c6ee3f1d62
nemoqwe08 <bjasinski8+github@gmail.com>
2017-04-06 03:59:55 +0200

    Before S arrays connection

--- GPU_DES_cracking/kernel.cu
+++ GPU_DES_cracking/kernel.cu
@@ -160,7 +160,54 @@ int IP[] = { 57, 49, 41, 33, 25, 17, 9, 1,
 60, 52, 44, 36, 28, 20, 12, 4,
 62, 54, 46, 38, 30, 22, 14, 6 };
 
+int E[] = { 31, 0, 1, 2, 3, 4,
+3, 4, 5, 6, 7, 8,
+7, 8, 9, 10, 11, 12,
+11, 12, 13, 14, 15, 16,
+15, 16, 17, 18, 19, 20,
+19, 20, 21, 22, 23, 24,
+23, 24, 25, 26, 27, 28,
+27, 28, 29, 30, 31, 0 };
 
+int S1[4][16] = { {14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7},
+{0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8},
+{4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0 },
+{15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13 } };
+
+int S2[4][16] = { {15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10},
+{3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5 },
+{0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15, },
+{13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9 } };
+
+int S3[4][16] = { {10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8},
+{13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1 },
+{13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7 },
+{1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12 } };
+
+int S4[4][16] = { {7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15},
+{13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9},
+{10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4},
+{3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14} };
+
+int S5[4][16] = { {2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9},
+{14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6},
+{4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14},
+{11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3} };
+
+int S6[4][16] = { {12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11},
+{10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8 },
+{9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6 },
+{4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13 } };
+
+int S7[4][16] = { {4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1},
+{13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6 },
+{1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2 },
+{6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12 } };
+
+int S8[4][16] = { {13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7},
+{1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2 },
+{7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8 },
+{2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11 } };
 
 void fun()
 {
@@ -344,6 +391,104 @@ void appendKeys(int leftKey[], int rightKey[], int key_size, int key_ret[])
 	}
 }
 
+
+void expand(int R[], int tab_ret[], int E[], int E_size)
+{
+
+	for (int i = 0; i < E_size; i++)
+		tab_ret[i] = R[E[i]];
+}
+
+void xor(int first_tab[], int second_tab[], int tab_size, int tab_ret[])
+{
+	for (int i = 0; i < tab_size; i++)
+		tab_ret[i] = (int)(!first_tab[i] != !second_tab[i]);
+
+}
+
+//-->
+long long binary2Decimal(int integer[], int tab_length)
+{
+	string int_string = "";
+
+	for (int i = 0; i < tab_length; i++)
+		int_string += to_string(integer[i]);
+	stringstream ss;
+	ss << int_string;
+	string str = ss.str();
+	unsigned long long value = std::stoull(str, 0, 2);
+	//std::cout << value << std::endl;
+	return value;
+}
+
+void f(int R[], int K[], int ret_tab[])
+{
+	int R_expanded[48];
+	expand(R, R_expanded, E, 48);
+	int xored[48];
+	xor (K, R, 48, xored);
+
+	for(int i = 0; i < 8; i++)
+	{
+		int row[4] = {0, 0, 0, 0 }, column[4] = {0, 0, 0, 0};
+		row[3] = xored[6 * i + 5];
+		row[2] = xored[6 * i];
+		column[0] = xored[6 * i + 1];
+		column[1] = xored[6 * i + 2];
+		column[2] = xored[6 * i + 3];
+		column[3] = xored[6 * i + 4];
+
+		S1[binary2Decimal(row, 4)][binary2Decimal(column, 4)];
+	}
+
+		
+}
+
+
+
+
+
+
+
+void messageEncode(int message_binary[], int message_size, int K[][48])
+{
+	int L[32], R[32];
+	for(int i = 0; i < message_size / 2; i++)
+	{
+		L[i] = message_binary[i];
+		R[i] = message_binary[i + message_size / 2];
+	}
+
+	int prev_L[32], prev_R[32];
+	for (int i = 0; i < message_size / 2; i++)
+	{
+		prev_L[i] = L[i];
+		prev_R[i] = R[i];
+	}
+
+
+
+	for(int i = 0; i < 16; i++)
+	{	
+		for (int j = 0; j < message_size / 2; j++)
+			L[j] = prev_R[i];
+
+		int tmp_f[32];
+		f(prev_R, K[i], tmp_f);
+		xor(prev_L, tmp_f, 32, R);
+
+
+		//przepisanie R i L do prev_R i prev_L
+		for(int i = 0; i < message_size / 2; i++)
+		{
+			prev_L[i] = L[i];
+			prev_R[i] = R[i];
+		}
+	}
+
+}
+
+
 //key_binary_ret should be 64 bit long
 string desEncyption(int message_binary[], int message_size,int key_binary[], int key_size)
 {
@@ -431,9 +576,10 @@ string desEncyption(int message_binary[], int message_size,int key_binary[], int
 	permutePC(message_binary, message_binary_ret, message_size, IP);
 
 	//DEBUG
-	for(int i = 0; i < message_size; i++)
-		cout << message_binary_ret[i];
+//	for(int i = 0; i < message_size; i++)
+//		cout << message_binary_ret[i];
 
+	messageEncode(message_binary_ret, message_size, K);
 
 	return "NOT IMPLEMENTED";
 }
@@ -499,6 +645,8 @@ int main()
 //	cout << str2.size() << endl;
 //	cout << str2.size() * CHAR_BIT << endl;
 	//TESTING
+
+
 
 	string message = "0123456789ABCDEF", key = "133457799BBCDFF1";
 	int message_binary[] = { 0,0,0,0, 0,0,0,1, 0,0,1,0, 0,0,1,1, 0,1,0,0, 0,1,0,1, 0,1,1,0, 0,1,1,1, 1,0,0,0, 1,0,0,1, 1,0,1,0, 1,0,1,1, 1,1,0,0, 1,1,0,1, 1,1,1,0, 1,1,1,1};
@@ -599,3 +747,47 @@ int main()
 //	std::cout << integral_to_binary_string(byte_array);
 //	//std::cin.get();
 //}
+
+
+
+
+//int binary2Decimal(int integer[])
+//{
+//	if (integer[0] == 0 && integer[1] == 0 && integer[2] == 0 && integer[3] == 0)
+//		return ;
+//	else if (integer[0] == && integer[1] == && integer[2] == && integer[3] == )
+//		return ;
+//	else if (integer[0] == && integer[1] == && integer[2] == && integer[3] == )
+//		return ;
+//	else if (integer[0] == && integer[1] == && integer[2] == && integer[3] == )
+//		return ;
+//	else if (integer[0] == && integer[1] == && integer[2] == && integer[3] == )
+//		return ;
+//	else if (integer[0] == && integer[1] == && integer[2] == && integer[3] == )
+//		return ;
+//	else if (integer[0] == && integer[1] == && integer[2] == && integer[3] == )
+//		return ;
+//	else if (integer[0] == && integer[1] == && integer[2] == && integer[3] == )
+//		return ;
+//	else if (integer[0] == && integer[1] == && integer[2] == && integer[3] == )
+//		return ;
+//	else if (integer[0] == && integer[1] == && integer[2] == && integer[3] == )
+//		return ;
+//	else if (integer[0] == && integer[1] == && integer[2] == && integer[3] == )
+//		return ;
+//	else if (integer[0] == && integer[1] == && integer[2] == && integer[3] == )
+//		return ;
+//	else if (integer[0] == && integer[1] == && integer[2] == && integer[3] == )
+//		return ;
+//	else if (integer[0] == && integer[1] == && integer[2] == && integer[3] == )
+//		return ;
+//	else if (integer[0] == && integer[1] == && integer[2] == && integer[3] == )
+//		return ;
+//	else if (integer[0] == && integer[1] == && integer[2] == && integer[3] == )
+//		return ;
+//	else if (integer[0] == && integer[1] == && integer[2] == && integer[3] == )
+//		return;
+//
+//		return -1000500;
+//}
+//
