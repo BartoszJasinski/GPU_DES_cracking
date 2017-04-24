d36b6072471065bb938d6ab48d84007025ebd4a4
nemoqwe08 <bjasinski8+github@gmail.com>
2017-04-24 17:11:21 +0200

    Array init works

--- GPU_DES_cracking/kernel.cu
+++ GPU_DES_cracking/kernel.cu
@@ -60,6 +60,9 @@ cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
 //{ 2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11 } };
 
 
+int PC_1_size = 56, shifts_size = 16, PC_2_size = 48, IP_size = 64, E_size = 48, S_size_1 = 8, S_size_2 = 4, S_size_3 = 16, P_size = 32, IP_1_size = 64;
+
+
 const int PC_1[56] = { 56, 48, 40, 32, 24, 16, 8, 0,
 57, 49, 41, 33, 25, 17, 9, 1,
 58, 50, 42, 34, 26, 18, 10, 2,
@@ -69,9 +72,9 @@ const int PC_1[56] = { 56, 48, 40, 32, 24, 16, 8, 0,
 28, 20, 12, 4, 27, 19, 11, 3};
 
 
-const int shifts[] = { 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1 };
+const int shifts[16] = { 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1 };
 
-const int PC_2[] = { 13, 16, 10, 23, 0, 4,
+const int PC_2[48] = { 13, 16, 10, 23, 0, 4,
 2, 27, 14, 5, 20, 9,
 22, 18, 11, 3, 25, 7,
 15, 6, 26, 19, 12, 1,
@@ -81,7 +84,7 @@ const int PC_2[] = { 13, 16, 10, 23, 0, 4,
 45, 41, 49, 35, 28, 31 };
 
 
-const int IP[] = { 57, 49, 41, 33, 25, 17, 9, 1,
+const int IP[64] = { 57, 49, 41, 33, 25, 17, 9, 1,
 59, 51, 43, 35, 27, 19, 11, 3,
 61, 53, 45, 37, 29, 21, 13, 5,
 63, 55, 47, 39, 31, 23, 15, 7,
@@ -90,7 +93,7 @@ const int IP[] = { 57, 49, 41, 33, 25, 17, 9, 1,
 60, 52, 44, 36, 28, 20, 12, 4,
 62, 54, 46, 38, 30, 22, 14, 6 };
 
-const int E[] = { 31, 0, 1, 2, 3, 4,
+const int E[48] = { 31, 0, 1, 2, 3, 4,
 3, 4, 5, 6, 7, 8,
 7, 8, 9, 10, 11, 12,
 11, 12, 13, 14, 15, 16,
@@ -133,14 +136,14 @@ const int S[8][4][16] = { { {14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 
 {2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11 } }
 };
 
-const int P[] ={15, 6, 19, 20,
+const int P[] = { 15, 6, 19, 20,
 	28, 11, 27, 16,
 	0, 14, 22, 25,
 	4, 17, 30, 9,
 	1, 7, 23, 13,
 	31, 26, 2, 8,
 	18, 12, 29, 5,
-	21, 10, 3, 24};
+	21, 10, 3, 24 };
 
 const int IP_1[] = {
 39, 7, 47, 15, 55, 23, 63, 31,
@@ -151,6 +154,103 @@ const int IP_1[] = {
 34, 2, 42, 10, 50, 18, 58, 26,
 33, 1, 41, 9, 49, 17, 57, 25,
 32, 0, 40, 8, 48, 16, 56, 24 };
+
+
+
+
+__constant__ int d_PC_1[56] = { 56, 48, 40, 32, 24, 16, 8, 0,
+57, 49, 41, 33, 25, 17, 9, 1,
+58, 50, 42, 34, 26, 18, 10, 2,
+59, 51, 43, 35, 62, 54, 46, 38,
+30, 22, 14, 6, 61, 53, 45, 37,
+29, 21, 13, 5, 60, 52, 44, 36,
+28, 20, 12, 4, 27, 19, 11, 3 };
+
+
+__constant__ int d_shifts[] = { 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1 };
+
+__constant__ int d_PC_2[] = { 13, 16, 10, 23, 0, 4,
+2, 27, 14, 5, 20, 9,
+22, 18, 11, 3, 25, 7,
+15, 6, 26, 19, 12, 1,
+40, 51, 30, 36, 46, 54,
+29, 39, 50, 44, 32, 47,
+43, 48, 38, 55, 33, 52,
+45, 41, 49, 35, 28, 31 };
+
+
+__constant__ int d_IP[] = { 57, 49, 41, 33, 25, 17, 9, 1,
+59, 51, 43, 35, 27, 19, 11, 3,
+61, 53, 45, 37, 29, 21, 13, 5,
+63, 55, 47, 39, 31, 23, 15, 7,
+56, 48, 40, 32, 24, 16,  8, 0,
+58, 50, 42, 34, 26, 18, 10, 2,
+60, 52, 44, 36, 28, 20, 12, 4,
+62, 54, 46, 38, 30, 22, 14, 6 };
+
+__constant__ int d_E[] = { 31, 0, 1, 2, 3, 4,
+3, 4, 5, 6, 7, 8,
+7, 8, 9, 10, 11, 12,
+11, 12, 13, 14, 15, 16,
+15, 16, 17, 18, 19, 20,
+19, 20, 21, 22, 23, 24,
+23, 24, 25, 26, 27, 28,
+27, 28, 29, 30, 31, 0 };
+
+__constant__ int d_S[8][4][16] = { { { 14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7 },
+{ 0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8 },
+{ 4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0 },
+{ 15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13 } },
+{ { 15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10 },
+{ 3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5 },
+{ 0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15 },
+{ 13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9 } },
+{ { 10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8 },
+{ 13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1 },
+{ 13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7 },
+{ 1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12 } },
+{ { 7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15 },
+{ 13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9 },
+{ 10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4 },
+{ 3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14 } },
+{ { 2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9 },
+{ 14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6 },
+{ 4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14 },
+{ 11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3 } },
+{ { 12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11 },
+{ 10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8 },
+{ 9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6 },
+{ 4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13 } },
+{ { 4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1 },
+{ 13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6 },
+{ 1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2 },
+{ 6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12 } },
+{ { 13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7 },
+{ 1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2 },
+{ 7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8 },
+{ 2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11 } }
+};
+
+__constant__ int d_P[] = { 15, 6, 19, 20,
+28, 11, 27, 16,
+0, 14, 22, 25,
+4, 17, 30, 9,
+1, 7, 23, 13,
+31, 26, 2, 8,
+18, 12, 29, 5,
+21, 10, 3, 24 };
+
+__constant__ int d_IP_1[] = {
+	39, 7, 47, 15, 55, 23, 63, 31,
+	38, 6, 46, 14, 54, 22, 62, 30,
+	37, 5, 45, 13, 53, 21, 61, 29,
+	36, 4, 44, 12, 52, 20, 60, 28,
+	35, 3, 43, 11, 51, 19, 59, 27,
+	34, 2, 42, 10, 50, 18, 58, 26,
+	33, 1, 41, 9, 49, 17, 57, 25,
+	32, 0, 40, 8, 48, 16, 56, 24 };
+
+
 
 void fun()
 {
@@ -433,7 +533,7 @@ void messageEncode(int message_binary[], int message_size, int K[][48], int msg_
 
 
 //key_binary_ret should be 64 bit long
-__host__ __device__ void desEncyption(int message_binary[], int message_size, int key_binary[], int key_size, int msg_ret[])
+__host__ __device__ void desEncryption(int message_binary[], int message_size, int key_binary[], int key_size, int msg_ret[])
 {
 	int des_block_size_bytes = 8;
 	int des_block_size_bits = 64;
@@ -665,7 +765,7 @@ string getHexStringFromBinaryString(string sHex)
 }
 
 
-string desEncyption(string message2Encrypt, string key, DesStringBase base)
+string desEncryption(string message2Encrypt, string key, DesStringBase base)
 {
 	//TODO implement different bases
 	string str_message = hex2Bin(message2Encrypt);
@@ -684,7 +784,7 @@ string desEncyption(string message2Encrypt, string key, DesStringBase base)
 	}
 
 	int msg_ret[64];
-	desEncyption(message_binary, message_binary_size, key_binary, key.size(), msg_ret);
+	desEncryption(message_binary, message_binary_size, key_binary, key.size(), msg_ret);
 	//DEBUG
 	//	for (int i = 0; i < 64; i++)
 	//	{
@@ -721,10 +821,7 @@ __host__ __device__ bool compareArrays(int message[], int cyphertext[])
 	return true;
 }
 
-__global__ void TEST1()
-{
-	printf("%s", "TEST");
-}
+
 
 __global__ 
 void crackDes(int message_binary[], int cyphertext_binary[], int message_binary_size)
@@ -746,7 +843,7 @@ void crackDes(int message_binary[], int cyphertext_binary[], int message_binary_
 	
 	printf("%s\n", "BEFORE desEncryption");
 
-	desEncyption(message_binary, message_binary_size, possible_key_binary, 16, msg_ret);
+	desEncryption(message_binary, message_binary_size, possible_key_binary, 16, msg_ret);
 
 	printf("%s\n", "AFTER desEncryption");
 
@@ -783,7 +880,7 @@ void IReportU(int message_binary[], int cyphertext_binary[], int message_binary_
 
 	printf("%s\n", "BEFORE desEncryption");
 
-	desEncyption(message_binary, message_binary_size, possible_key_binary, 16, msg_ret);
+	desEncryption(message_binary, message_binary_size, possible_key_binary, 16, msg_ret);
 
 	printf("%s\n", "AFTER desEncryption");
 
@@ -838,14 +935,123 @@ void crackDes(string message, string cyphertext)
 
 }
 
+__global__
+void desEncryption(int message_binary[], int key_binary[], int message_binary_size, int msg_ret[])
+{
+	//DEBUG
+	printf("%s\n", "before DEBUG __global__ desEncryption");
+	for (int i = 0; i < message_binary_size; ++i)
+	{
+		printf("%i", message_binary[i]);
+	}
+	printf("%s\n", "after DEBUG __global__ desEncryption");
+
+	//int msg_ret[64];
+//	printf("%s\n", "BEFORE desEncryption");
+	desEncryption(message_binary, message_binary_size, key_binary, 16, msg_ret);
+	
+
+}
+__host__
+string desEncryption(string message, string key, char cyphertext[])
+{
+	string str_message = hex2Bin(message);
+	int h_message_binary_size = 64;
+	int h_message_binary[64];
+	str2Int(str_message, h_message_binary, h_message_binary_size);
+
+	string str_key = hex2Bin(key);
+	int h_key_binary_size = 64;
+	int h_key_binary[64];
+	str2Int(str_key, h_key_binary, h_key_binary_size);
+
+	int* d_message_binary = 0;
+	cudaMalloc((void**)&d_message_binary, h_message_binary_size * sizeof(int));
+	cudaMemcpy(d_message_binary, h_message_binary, h_message_binary_size * sizeof(int), cudaMemcpyHostToDevice);
+
+	int* d_key_binary = 0;
+	cudaMalloc((void**)&d_key_binary, h_key_binary_size * sizeof(int));
+	cudaMemcpy(d_key_binary, h_key_binary, h_key_binary_size * sizeof(int), cudaMemcpyHostToDevice);
+
+	int* d_msg_ret;
+	cudaMalloc((void**)&d_msg_ret, 64 * sizeof(int));
+
+	//DEBUG
+	printf("%s\n", "before DEBUG __host__ desEncryption");
+	for (int i = 0; i < 64; ++i)
+	{
+		printf("%i", h_message_binary[i]);
+	}
+	printf("%s\n", "after DEBUG __host__ desEncryption");
+
+	desEncryption<<<1, 1>>>(d_message_binary, d_key_binary, 64, d_msg_ret);
+	cudaDeviceSynchronize();
+
+	int* h_msg_ret = (int*)malloc(64 * sizeof(int));
+	cudaMemcpy(h_msg_ret, d_msg_ret, 64 * sizeof(int), cudaMemcpyDeviceToHost);
+	//DEBUG
+//	printf("");
+	printf("\n%s\n", "before DEBUG __host__ desEncryption H_MSG_RET");
+	for (int i = 0; i < 64; ++i)
+	{
+		printf("%i", h_msg_ret[i]);
+	}
+	printf("%s\n", "after DEBUG __host__ desEncryption H_MSG_RET");
+
+
+	string binary;
+	for (int i = 0; i < 64; i++)
+		binary.push_back(std::to_string(h_msg_ret[i]).c_str()[0]);
+	//DEBUG
+	//cout << binary;
+
+	return getHexStringFromBinaryString(binary);
+		
+}
+
+
+void initArrays()
+{
+	cudaMemcpyToSymbol(d_PC_1, PC_1, PC_1_size * sizeof(int));
+	cudaMemcpyToSymbol(d_shifts, shifts, shifts_size * sizeof(int));
+	cudaMemcpyToSymbol(d_PC_2, PC_2, PC_2_size * sizeof(int));
+	cudaMemcpyToSymbol(d_IP, IP, IP_size * sizeof(int));
+	cudaMemcpyToSymbol(d_E, E, E_size * sizeof(int));
+	cudaMemcpyToSymbol(d_P, P, P_size * sizeof(int));
+	cudaMemcpyToSymbol(d_S, S, S_size_1 * S_size_2 * S_size_3 * sizeof(int));
+	cudaMemcpyToSymbol(d_IP_1, IP_1, IP_1_size * sizeof(int));
+
+}
+
+//__global__ void arrayCheck(int size1, int size2, int size3)
+//{
+//	for (int i = 0; i < size1; i++)
+//	{
+//		for (int j = 0; j < size2; j++)
+//		{
+//			for (int k = 0; k < size3; ++k)
+//			{
+//				printf("%i,", d_S[i][j][k]);
+//			}
+//			printf("\n");
+//		}
+//		printf("\n\n");
+//	}
+//		
+//}
+
+
 int main()
 {
-	string message = "0123456789ABCDEF", key = "0000000000000000";
-	string cyphertext = desEncyption(message, key, DesStringBase::Hex);
-	cout << cyphertext << "\n";
-	//cyphertext = ""
-	crackDes(message, cyphertext);
+	initArrays();
+	arrayCheck << <1, 1 >> > (S_size_1, S_size_2, S_size_3);
 	cudaDeviceSynchronize();
+//	string message = "0123456789ABCDEF", key = "0000000000000000";
+//	char cyphertext[64];
+//	string ct = desEncryption(message, key, cyphertext);
+//	cout << ct << "\n";
+//	crackDes(message, cyphertext);
+//	cudaDeviceSynchronize();
 
 
 
@@ -1057,7 +1263,7 @@ int main()
 ////	int key_binary[] = { 0,0,0,1,0,0,1,1, 0,0,1,1,0,1,0,0, 0,1,0,1,0,1,1,1, 0,1,1,1,1,0,0,1, 1,0,0,1,1,0,1,1, 1,0,1,1,1,1,0,0, 1,1,0,1,1,1,1,1, 1,1,1,1,0,0,0,1 };
 //	time_t start = time(nullptr);
 //	for(int i = 0; i < 2000; i++)
-//		string cyphertext = desEncyption(message, key, DesStringBase::Hex);
+//		string cyphertext = desEncryption(message, key, DesStringBase::Hex);
 //	time_t stop = time(nullptr);
 //
 //	cout << "\n\n\n" << difftime(stop, start);
